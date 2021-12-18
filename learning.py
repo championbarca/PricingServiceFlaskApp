0f25c1759ef34e158323999e80b994cb4922724c
championbarca <vineetmhapsekar@yahoo.co.in>
2021-12-18 18:33:53 +0800

    first commit

---
+++ learning.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+learning_blueprint = Blueprint("learning", __name__)
+
+
+@learning_blueprint.route('/<string:name>')
+def home(name):
+    return f"Hello, {name}!!!"
+
