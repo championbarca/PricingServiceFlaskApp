1171a0d45da6366bda894a0f90ac347a96da1f5f
championbarca <vineetmhapsekar@yahoo.co.in>
2021-12-19 16:42:39 +0800

    user registration

---
+++ models/user/__init__.py
@@ -0,0 +1,2 @@
+from models.user.user import User
+from models.user.errors as UserErrors
---
+++ models/user/user.py
@@ -0,0 +1,39 @@
+import uuid
+from dataclasses import dataclass, field
+from typing import Dict
+from models.model import Model
+import models.user.errors as UserErrors
+
+@dataclass
+class User(Model):
+    collection:str = field(init=False, default="users")
+    email:str
+    password:str = field(repr=False)
+    _id:str = field(default_factory=lambda: uuid.uuid4().hex)
+    
+    @classmethod
+    def find_by_email(cls, email:str) -> "User":
+        try:
+            return cls.find_one_by('email', email)
+        except TypeError:
+            raise UserErrors.UserNotFoundError("A user with e-mail was not found")
+        
+    @classmethod
+    def register_user(cls, email:str, password:str) -> "User":
+        if not Utils.email_is_valid(email):
+            raise UserErrors.InvalidEmailError("The e-mail does not have the right format.")
+        
+        try:
+            user = cls.find_by_email(email)
+            raise UserErrors.UserAlreadyRegisteredError("The e-mail you used to register already exists.")
+        except UserErrors.UserNotFoundError:
+            User(email, password).save_to_mongo()
+
+        return True
+
+    def json(self) -> Dict:
+        return {
+            "_id": self._id,
+            "email": self.email,
+            "password": self.password
+        }
