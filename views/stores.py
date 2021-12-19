6463dde9be109d1f053e13ce5b8d9fa09ac2e89c
championbarca <vineetmhapsekar@yahoo.co.in>
2021-12-19 15:03:43 +0800

    delete store alert

--- views/stores.py
+++ views/stores.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.store import Store
 
 store_blueprint = Blueprint("stores", __name__)
@@ -20,3 +20,23 @@ def create_store():
         Store(name, url_prefix, tag_name, query).save_to_mongo()
         
     return render_template("store/new_store.html")
+
+
+@store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
+def edit_alert(store_id):
+    store = Store.get_by_id(store_id)
+    if request.method == "POST":
+        store.url_prefix = float(request.form["url_prefix"])
+        store.tag_name = float(request.form["tag_name"])
+        store.query = float(request.form["query"])
+        
+        store.save_to_mongo()
+        return redirect(url_for('.index'))
+    
+    return render_template("stores/edit_store.html", store=store)
+
+
+@store_blueprint.route('/delete/<string:store_id>')
+def delete_store(store_id):
+    Store.get_by_id(store_id).remove_from_mongo()
+    return redirect(url_for('.index'))
