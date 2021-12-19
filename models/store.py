774e5af724ffc6a1cb5962da65feaaad470181a9
championbarca <vineetmhapsekar@yahoo.co.in>
2021-12-19 12:00:29 +0800

    store changes

--- models/store.py
+++ models/store.py
@@ -1,5 +1,6 @@
 from typing import Dict
 import uuid
+import re
 from models.model import Model
 
 
@@ -13,3 +14,31 @@ class Store(Model):
         self.tag_name = tag_name
         self.query = query
         self._id = _id or uuid.uuid4().hex
+
+    def json(self) -> Dict:
+        return {
+            "_id": self._id,
+            "name": self.name,
+            "url_prefix": self.url_prefix,
+            "tag_name": self.tag_name,
+            "query": self.query
+        }
+
+    @classmethod
+    def get_by_name(cls, store_name:str) -> "Store":
+        return cls.find_one_by("name", store_name)
+
+    @classmethod
+    def get_by_url_prefix(cls, url_prefix:str) -> "Store":
+        url_regex = {"$regex": "^{}".format(url_prefix)}
+        return cls.find_one_by("url_prefix", url_regex)
+
+    @classmethod
+    def find_by_url(cls, url:str) -> "Store":
+        """
+        Return a store url from item url
+        """
+        pattern = re.compile(r"https?://.*?/")
+        match = pattern.search(url)
+        url_prefix = match.group(1)
+        return cls.get_by_url_prefix(url_prefix)
